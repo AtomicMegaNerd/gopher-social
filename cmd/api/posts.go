4c4167bd75d60d9cd4d3ab8fd67974d67e87014e
Chris Dunphy <chris.dunphy@fglsports.com>
2025-07-09 16:05:02 -0600

    Fixes to posts

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -14,7 +14,7 @@ type CreatePostPayload struct {
 
 func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request) {
 	var payload CreatePostPayload
-	if err := readJSON(w, r, payload); err != nil {
+	if err := readJSON(w, r, &payload); err != nil {
 		writeJSONError(w, http.StatusBadRequest, err.Error())
 	}
 	userId := 1 // TODO: This should be replaced with actual user ID extraction logic
