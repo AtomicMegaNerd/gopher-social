313db234b4d4cdc536c3f800655a881bf955259d
Chris Dunphy <chris.dunphy@fglsports.com>
2025-10-15 15:34:26 -0600

    Add the ability to create comments

---
+++ cmd/api/comments.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/atomicmeganerd/rcd-gopher-social/internal/store"
+)
+
+type CreateCommentPayload struct {
+	Content string `json:"content" validate:"required,max=1000"`
+}
+
+func (app *application) createCommentHandler(w http.ResponseWriter, r *http.Request) {
+
+	post := getPostFromContext(r)
+	userId := 1 // TODO: This should be replaced with actual user ID extraction logic
+
+	var payload CreateCommentPayload
+	if err := readJSON(w, r, &payload); err != nil {
+		app.badRequestError(w, r, err)
+		return
+	}
+
+	if err := Validate.Struct(payload); err != nil {
+		app.badRequestError(w, r, err)
+		return
+	}
+
+	comment := &store.Comment{
+		PostID:  post.ID,
+		UserID:  int64(userId),
+		Content: payload.Content,
+	}
+
+	if err := app.store.Comments.Create(r.Context(), comment); err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+
+	if err := app.jsonResponse(w, http.StatusCreated, comment); err != nil {
+		app.internalServerError(w, r, err)
+	}
+}
