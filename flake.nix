018f394c8e69790a2e2fb6dbf76f101c29329bc5
Chris Dunphy <chris@megaparsec.ca>
2026-02-12 06:57:09 -0700

    Do not use flake-utils

--- flake.nix
+++ flake.nix
@@ -2,42 +2,51 @@
   description = "A udemy course for backend development with Go";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
     {
       self,
       nixpkgs,
-      flake-utils,
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        # See https://github.com/golang-migrate/migrate/issues/1279
-        go-migrate-pkg = pkgs.go-migrate.overrideAttrs (oldAttrs: {
-          tags = [ "postgres" ];
-        });
-      in
-      {
-        devShell = pkgs.mkShell {
-          # The packages we need for this project
-          buildInputs = [
-            pkgs.go_1_25
-            pkgs.go-tools
-            pkgs.gopls
-            pkgs.golangci-lint
-            pkgs.go-task
-            go-migrate-pkg
-            pkgs.air
-            pkgs.rainfrog
-            pkgs.posting
-            pkgs.bash-language-server
-            pkgs.docker-language-server
-            pkgs.yaml-language-server
-          ];
+    let
+      systems = {
+        linux = "x86_64-linux";
+        darwin = "aarch64-darwin";
+      };
+      buildPkgsConf =
+        system: pkg_src:
+        import pkg_src {
+          inherit system;
+          config.allowUnfree = true;
         };
-      }
-    );
+    in
+    {
+      devShells = nixpkgs.lib.genAttrs (builtins.attrValues systems) (
+        system:
+        let
+          pkgs = buildPkgsConf system nixpkgs;
+        in
+        {
+          default = pkgs.mkShell {
+            # The packages we need for this project
+            buildInputs = [
+              pkgs.go_1_25
+              pkgs.go-tools
+              pkgs.gopls
+              pkgs.golangci-lint
+              pkgs.go-task
+              pkgs.go-migrate
+              pkgs.air
+              pkgs.rainfrog
+              pkgs.posting
+              pkgs.bash-language-server
+              pkgs.docker-language-server
+              pkgs.yaml-language-server
+              pkgs.efm-langserver
+            ];
+          };
+        }
+      );
+    };
 }
